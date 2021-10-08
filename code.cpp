889fb64b1fcdf954be0a96fbff95d5d42410833b
Rohit Kumar Agrahari <54462758+ROHITKUMARAGR@users.noreply.github.com>
2021-10-09 00:48:31 +0530

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -1,47 +1,36 @@
 //Implemented by Anand,Mayank,Rohit.
 #include<bits/stdc++.h>
-#define ll long long 
-#define pb push_back
-#define all(n) n.begin(),n.end() 
-#define vec vector<int>
-#define fastio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-#define vp(it,arr)  for(auto it:arr){cout<<it<<;}
-#define LOOP(i,a,b) for(int i=a;i<b;i++)
 using namespace std;
 //Class for the Charging Station
 class EVS{  
  public:
     int station_number;
     int charging_port;
-    deque<pair<int,int>>waiting;
+    map<int,int>waiting;
     EVS(int a,int charging_port){
         station_number=a;
         this->charging_port=charging_port;
     }
    
-
+// This function is to keep updated the waiting map. 
 void update_the_waiting(int current_time){
      auto it=waiting.begin();
-     while(it->second<current_time){
-         waiting.pop_front();
+     while(it->second<current_time&&it!=waiting.end()){
+         waiting.erase(it->first);
+         it++;
      }
 }
-
+pair<int,int>available_time_slot;//This will store the next time slot availabale.
 bool check_the_waiting_queue(int start_time,int end_time){
-    if(check_CS_isempty()){
-               for(auto it=waiting.begin();it!=waiting.end();it++){
-                   if(it->first>end_time){
-                       return true;
-                   }
-                   if(it->second<start_time){
-                       return true;
-                   }
-
-               }
-               return false;
-    }else{
-        return false;
+    for(auto it=waiting.begin();it!=waiting.end();it++){
+          if(it->first>start_time&&it->second<end_time){
+              available_time_slot.first=it->first;
+              available_time_slot.second=it->second;
+              return true;
+          }
+          if(it->first>start_time)
     }
+   
 }
  
 bool book_the_CS(int starting_tinme,int end_time){
@@ -99,19 +88,30 @@ unordered_map<int,node>network;//This is our core graph
 int i;
 int node_num=0;
 do{
-      cout<<"Press 1 if you want to add some new Charging Station"<<endl;
-      cout<<"Press 2 if you want to do travel some from one point to the other"<<endl;
-      cout<<"Press 3 for just Charging the EV in nearby Places according to your EV's remaining charge"<<endl;
-      cin>>i;
-      if(i==1){
-       cout<<"In Order to add some Charging Station you should provide the number of charging ports will be present there"<<endl;
-       int ports;
-       cin>>ports;
-       EVS newer_node(node_num+1,ports);
-       arru.push_back(newer_node);
-       node_num++;
-       cout<<"Enter the other nodes distances from"<<endl;
-      }
+   cout<<"Press 1 if you want to add some new Charging Station"<<endl;
+   cout<<"Press 2 if you want to do travel some from one point to the other"<<endl;
+   cout<<"Press 3 for just Charging the EV in nearby Places according to your EV's remaining charge"<<endl;
+   cin>>i;
+   if(i==1){
+   cout<<"In Order to add some Charging Station you should provide the number of charging ports will be present there"<<endl;
+   int ports;
+   cin>>ports;
+   EVS newer_node(arru.size(),ports);
+   arru.push_back(newer_node);
+   node n1;
+   network.insert({arru.size(),n1});
+   cout<<"Enter the number of vertices which are adjacent to the given vertices"<<endl;
+   int nv;
+   cin>>nv;
+   while(nv--){
+       cout<<"Enter the nodes nearby value and then the weight associated with it"<<endl;
+       int node,weight;
+       cin>>node>>weight;
+        network[arru.size()].nbrs.insert({node,weight});
+   }
+   cout<<"Nodes have been succesfully inserted"<<endl;
+   
+}
       if(i==2){
               cout<<"Enter the source point and the destination point"<<endl;
               int sp,dp;
@@ -135,6 +135,8 @@ do{
                     nearest_distance_CS.insert({value2,*it});
                     }
                 }
+
+            map<int,int>next_time_slots;
             if(nearest_distance_CS.size()==0){
                   cout<<"Sorry we cannot help you beacuse you remaining charge cannot take you to any CS"<<endl;
             }else{
@@ -144,8 +146,8 @@ do{
                   int end_time=((100-charge_percent)/100)*4;
                   for(auto it=nearest_distance_CS.begin();it!=nearest_distance_CS.end();it++){
                        arru[it->second].update_the_waiting(current_time);
-                       
-                  }
+                      
+                    }
             }
               
 
